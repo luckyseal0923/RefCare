acd984860250ca85d40942d3490b9ac4b7e092bc
luckyseal0923 <luckyseal0923@gmail.com>
2026-06-29 12:21:47 +0800

    data: update facilities.json and sql scripts from 機構名單列表-修訂.xlsx

---
+++ update_facilities_line_ids.sql
@@ -0,0 +1,44 @@
+-- ==========================================
+-- 批次更新機構的 LINE Group ID
+-- 總計更新數量: 36 筆
+-- ==========================================
+
+-- Reset all line_group_ids first
+UPDATE facilities SET line_group_id = NULL;
+
+UPDATE facilities SET line_group_id = 'Cd47f830e89647e4022430893501739cc' WHERE facility_name = '智慧⻑照有限公司附設臺北市私立愛關懷居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C686e7df24f8dd25cec549f593218c88a' WHERE facility_name = '愛馨居服有限公司附設臺北市私立愛馨居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cd52c555e6ba077a745035821918fcdb6' WHERE facility_name = '恩揚有限公司附設臺北市私立恩揚居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cfe8c9f9a216b0c9e2e7f20bdad9e4448' WHERE facility_name = '社團法⼈中華⻑照協會附設臺北市私立永樂居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C6da65953e20279ed3491cc56e649a7a8' WHERE facility_name = '寶安健康有限公司附設臺北市私立鑫安居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Ca188f348c1a595f74722549e51e59a9e' WHERE facility_name = '惠安護康科技有限公司附設臺北市私立惠安居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C67cb83e43545d8b19b6383271627033d' WHERE facility_name = '優活健康顧問有限公司附設臺北市私立優活居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C7838c096898e7df27a071ddcb724df04' WHERE facility_name = '北醫⼤管理顧問股份有限公司附設私立杏芳居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Ceaf6955a2730559d541912cad0d4198' WHERE facility_name = '東和居家有限公司附設臺北市私立東正居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C8cf9abd852f578b18a078765ee314931' WHERE facility_name = '臺北市私立安家居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C499437c243b107d77e837283812a0ea3' WHERE facility_name = '泉益居家有限公司附設臺北市私立泉益居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cba661b0f4e40fa75d6898c902ef77c9c' WHERE facility_name = '端豈資產管理股份有限公司附設臺北市私立端豈居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C8895bd7a2af6af75139019dbcbc115b7' WHERE facility_name = '臺北市私立前瞻居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cf036dadd2d518bfd273ae2eb4ecc17da' WHERE facility_name = '臺北市私立祝福居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C7ef1f9b37099023069066001f48a9678' WHERE facility_name = '美樺股份有限公司附設私立美美居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C4b5d6d30e854ea0b4160a4070d8324da' WHERE facility_name = '謙和園健康福祉有限公司附設臺北市私立溫邸家居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cbd450b588524515a29594295b229eca7' WHERE facility_name = '銀享全球股份有限公司附設臺北市私立銀享居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C26fafc5cbc983fc5eaba4a24a65202d0' WHERE facility_name = '財團法人恆安社會福利慈善事業基金會附設臺北市私立恆安居家式服務類⻑期照顧服務機構';
+UPDATE facilities SET line_group_id = 'Cc83669c6b803e9ea627f3f023bac00ff' WHERE facility_name = '財團法⼈獎卿護理展望基⾦會私立臺北居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C2773b617bdfbde3f540bd4e132833c64' WHERE facility_name = '臺北市私立老爺居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cd3de4ba16ebd95bb75e77ee13c6b3ef6' WHERE facility_name = '康捷居家⻑照有限公司附設臺北市私立康捷居家式服務類⻑期照顧服務機構';
+UPDATE facilities SET line_group_id = 'C13d99ec130550d46e9e96927d2db4928' WHERE facility_name = '宗展股份有限公司附設臺北市私立辰⼼居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C00e64413d5d95ea68276e2b581b57894' WHERE facility_name = '臺北市私立悠樂活居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C4221288e4d2545977f38442a1f2a08a0' WHERE facility_name = '中化銀髮事業股份有限公司附設臺北市私立中化銀髮居家式服務類⻑期照顧服務機構';
+UPDATE facilities SET line_group_id = 'Cbf43729de4eb49e272891a5094b6d78c' WHERE facility_name = '新北市私立永安居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C446d497a605c05511e2613f66b75e444' WHERE facility_name = '新北市私立全曜居家式服務類⻑期照顧服務機構';
+UPDATE facilities SET line_group_id = 'Ccc92aabea1e089f1827cafc4cacf7b5e' WHERE facility_name = '皓仁健康照護事業有限公司附設臺北市私立皓仁居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C4be86a641e4c7afcf4228aae54cdd56f' WHERE facility_name = '凱安居家有限公司附設臺北市私立凱安居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C2d216b5254e71c41fe5b13955de71f56' WHERE facility_name = '遠曜居家有限公司附設臺北市私立我在居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C9300df8bd4886e3ea7f6ad280c6e230d' WHERE facility_name = '百佳健康事業有限公司附設臺北市私立百佳居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C4e36f2d7243efa4ff89eb021808a53bc' WHERE facility_name = '翔伶事業有限公司附設臺北市私立⽇夏居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C3218624197b7f231303ae82ece336f72' WHERE facility_name = '潤⽥健康有限公司附設臺北市私立潤⽥居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C809aa220968eb1ad539ebb7287e1e622' WHERE facility_name = '耀⽣⻑期照顧服務股份有限公司附設臺北市私立耀⽣居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C406d1a365e11a37ee896b59fb93a67e6' WHERE facility_name = '安德利互聯網股份有限公司附設臺北市私立安德利居家⻑照機構';
+UPDATE facilities SET line_group_id = 'Cbb8348ae55e44978157dff1258a0b1bd' WHERE facility_name = '臺北市私立樂陽居家⻑照機構';
+UPDATE facilities SET line_group_id = 'C5c0866e1091019cf3b45e8269a1f9cd6' WHERE facility_name = '新北市私立旺福居家⻑照機構';
